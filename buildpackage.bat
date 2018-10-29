9b7802ca2510b7db0129b532fdc3a8a07e556abf
Luuk Sommers <luuk.sommers@gmail.com>
2018-10-29 22:07:14 +0100

    Update build script

--- buildpackage.bat
+++ buildpackage.bat
@@ -1,4 +1,5 @@
 cd src
 ..\tools\nuget\nuget.exe pack SevenZipSharp.Interop.nuspec -OutputDirectory ..\artifacts
+..\tools\nuget\nuget.exe pack SevenZipSharp.Interop.NoReferences.nuspec -OutputDirectory ..\artifacts
 cd ..
 pause
